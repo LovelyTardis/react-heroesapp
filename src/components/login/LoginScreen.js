e048906c530c580ae98a17c693c0ccde1af47f67
Albert Chao <albert.tardis05@gmail.com>
2022-03-16 17:51:37 +0100

    heroes app updated

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,38 +1,33 @@
-import React, { useContext, useReducer } from 'react'
-import { useNavigate } from 'react-router-dom';
-import { AuthContext } from '../../auth/authContext';
-import { authReducer } from '../../auth/authReducer';
-import { types } from '../../types/types';
+import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../auth/authContext";
+import { types } from "../../types/types";
 
 export const LoginScreen = () => {
+  const { dispatch } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const lastPath = localStorage.getItem("lastPath") || "/";
 
-    const {dispatch} = useContext(AuthContext);
-    const navigate = useNavigate();
-    const lastPath = localStorage.getItem('lastPath') || '/';
+  const handleLogin = () => {
+    const action = {
+      type: types.login,
+      payload: {
+        name: "Albert",
+      },
+    };
+    dispatch(action);
+    navigate(lastPath, {
+      replace: true,
+    });
+  };
+  return (
+    <div className="container mt-5">
+      <h1>LoginScreen</h1>
+      <hr />
 
-    const handleLogin = () => {
-      const action = {
-        type: types.login,
-        payload: {
-          name: 'Albert',
-        }
-      }
-      dispatch(action);
-      navigate(lastPath, {
-        replace: true
-      });
-    }
-    return (
-      <div className='container mt-5'>
-          <h1>LoginScreen</h1>
-          <hr />
-
-          <button
-            className='btn btn-primary'
-            onClick={handleLogin}
-          >
-            Login
-          </button>
-      </div>
-    )
-}
+      <button className="btn btn-primary" onClick={handleLogin}>
+        Login
+      </button>
+    </div>
+  );
+};
